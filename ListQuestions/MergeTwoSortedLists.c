624a38e74aacb49ee88a7bf8452995d701f809c3
Can Dogrul <candogrull@hotmail.com>
2024-08-02 15:13:47 +0200

    modified:   ListQuestions/MergeTwoSortedLists.c

--- ListQuestions/MergeTwoSortedLists.c
+++ ListQuestions/MergeTwoSortedLists.c
@@ -1,35 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
 
-
-struct ListNode
+typedef struct ListNode
 {
     int val;
     struct ListNode *next;
-};
+} ListNode;
 
-struct ListNode *mergeTwoLists(struct ListNode *list1, struct ListNode *list2)
+ListNode *Node(int new_Data)
 {
-
-    // determining the listsizes
-
-    int list_size1 = sizeof(list1) / sizeof(list1[0]);
-    int list_size2 = sizeof(list2) / sizeof(list2[0]);
-    int listsizetot = list_size1 + list_size2;
-
-    // create the variables
-    int *matrix[2];
-    int holder[listsizetot];
-    int i,j;
-
-    matrix[0] = list1;
-    matrix[1] = list2;
-
-    i = 0;
-
-    while (i < listsizetot)
-    {
-        //here we must write an algorithm to conduct a research
-        
-
-
-    }
+    ListNode *new_Node = (ListNode *)malloc(sizeof(ListNode));
+    new_Node->val = new_Data;
+    new_Node->next = NULL;
+    return new_Node;
 }
+
+struct ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+{
+    // defining our dummy here
+    ListNode dummy;
+    dummy.next = NULL;
+    dummy.val = -1;
+
+    // pointing the tail to the dummy
+    ListNode *tail = &dummy;
+
+    // condition checks
+    if (list1->next == NULL && list2->next == NULL)
+    {
+        return NULL;
+    }
+
+    // the loop to check the values
+    while (list1->next != NULL || list2->next != NULL)
+    {
+        // makng the comparisons
+
+        if (list1->val <= list->val)
+        {
+            
+        }
+    }
