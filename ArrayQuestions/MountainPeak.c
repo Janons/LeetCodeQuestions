5786aed89afb7b653122c1937419cb494d16b8e5
Can Dogrul <candogrull@hotmail.com>
2024-08-03 14:59:18 +0200

    modified:   ArrayQuestions/MountainPeak.c

--- ArrayQuestions/MountainPeak.c
+++ ArrayQuestions/MountainPeak.c
@@ -6,37 +6,32 @@
 
 int peakIndexInMountainArray(int *arr, int arrSize);
 
-int peakIndexInMountainArray(int *arr, int arrSize)
-{
-
-    int r = arrSize-1;
+int peakIndexInMountainArray(int* arr, int arrSize) {
+    int r = arrSize - 1;
     int l = 0;
     int m = 0;
 
-    while (r >= l)
-    {
+    while (l < r) {
         m = (int)floor((r + l) / 2);
 
-        if (arr[m] > arr[m + 1] && arr[m] > arr[m - 1])
-        {
+        if (arr[m] > arr[m + 1] && arr[m] > arr[m - 1]) {
             return m;
         }
 
-        else if (arr[m] > arr[m + 1] && arr[m] < arr[m - 1])
-        {
+        else if (arr[m] > arr[m + 1]) {
             r = m - 1;
         }
 
-        else if (arr[m] > arr[m - 1] && arr[m] < arr[m + 1])
-        {
+        else {
             l = m + 1;
         }
     }
-    return m;
+    return l;
 }
 
+
 int main() {
-    int arr1[4] = {0, 10, 5, 2};
+    int arr1[3] = {0,1,0};
     int result;
 
     result = peakIndexInMountainArray(arr1, 4);
