2da3dad603d866e3159b84072763ccd451509fb2
Can Dogrul <candogrull@hotmail.com>
2024-10-11 11:09:45 +0200

    modified:   ArrayQuestions/RotateImage.c

--- ArrayQuestions/RotateImage.c
+++ ArrayQuestions/RotateImage.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void rotate(int **matrix, int matrixSize, int *matrixColSize);
 
-void rotate(int** matrix, int matrixSize, int* matrixColSize);
-
-
-int main(){
+int main()
+{
     return 0;
 }
 
-void rotate(int** matrix, int matrixSize, int* matrixColSize)
+void rotate(int **matrix, int matrixSize, int *matrixColSize)
 {
-    int i,j,n; //variables we will use to perform the rotation
+    int i, j, n; // variables we will use to perform the rotation
+    int **temp = (int**)malloc(matrixSize * sizeof(int*));
 
+    //memeory allocation for the rows
+    for ( int i =0; i< matrixSize; i++)
+    {
+        temp[i] = (int*)malloc(matrixSize * sizeof(int));    }
 
-//getting to the rows
-    for (i=0; i<matrixSize; i++){
-        for (j=0; j<matrixColSize; j ++)
+    // getting to the rows
+    for (i = 0; i < matrixSize; i++)
+    {
+        for (j = 0; j < matrixColSize; j++)
         {
-            //outer part
-            for ()
-
-            //other parts
-
-
-            //center
-
+            // outer part
+            temp = matrix;
+            matrix[i][j] = matrix[j][n-1-i];
+            matrix[j][n-1-i] = matrix[n-1-i][n-1-j];
+            matrix[n-1-i][n-1-j] =  matrix[n-1-j][i];
+            matrix[n-1-j][i] = temp;
         }
     }
 }
