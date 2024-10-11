6ba6b0cf2343765b83e3ebdab6572d48cda001fa
Can Dogrul <candogrull@hotmail.com>
2024-10-11 11:09:30 +0200

    modified:   ArrayQuestions/RotateImage.c

---
+++ ArrayQuestions/RotateImage.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void rotate(int** matrix, int matrixSize, int* matrixColSize);
+
+
+int main(){
+    return 0;
+}
+
+void rotate(int** matrix, int matrixSize, int* matrixColSize)
+{
+    int i,j,n; //variables we will use to perform the rotation
+
+
+//getting to the rows
+    for (i=0; i<matrixSize; i++){
+        for (j=0; j<matrixColSize; j ++)
+        {
+            //outer part
+            for ()
+
+            //other parts
+
+
+            //center
+
+        }
+    }
+}
